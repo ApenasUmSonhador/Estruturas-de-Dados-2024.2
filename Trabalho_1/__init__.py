cbaf4dbe955386fc9ea053e97888e3474c8de6fd
Arthur Nunes <arthurvininunes@gmail.com>
2024-11-21 21:36:50 -0300

    Criação de novo __init__

--- Trabalho_1/__init__.py
+++ Trabalho_1/__init__.py
@@ -5,6 +5,9 @@ setup()
 # Importa e executa os testes
 from test_stack import test_stack
 from test_converter import test_converter
+from test_calculator import test_calculator
 
-test_stack()
-test_converter()
+if (test_stack() and test_converter() and test_calculator()):
+    print("All tests passed in all Classes")
+else:
+    print("Some tests did'nt passed in all Classes")
